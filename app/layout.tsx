3f9d64edddedbe88741169c2ae691768ad2f7b2f
Bruno Miguel Antonio Santos <bruno@MacBookAir.lan>
2026-06-26 09:31:00 +0200

    Ajout de Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,23 @@
-import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css';
+import type { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'DataConverter Pro | Convertisseur CSV vers Excel Gratuit',
-  description: 'Convertissez vos fichiers CSV en tableaux lisibles et téléchargez-les en format Excel en un clic. Outil gratuit et rapide.',
-}
+  description: 'Convertissez vos fichiers CSV en tableaux lisibles et téléchargez-les en format Excel en un clic.',
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
